ce0f47689b77e06bc62bbf7c69b9b83fae946b8a
Jeremy Dyer <jdye64@gmail.com>
2016-07-07 13:16:30 -0400

    8GB of RAM was no where near enough to run all of the services in the environment … kept getting Java mmap memory errors. Trying 12GB and introduced uninstall script to create a fresh VirtualBox VM

--- Operations/nifi-playground/install_macosx.sh
+++ Operations/nifi-playground/install_macosx.sh
@@ -3,7 +3,7 @@ echo "Setting up nifi-playground VirtualBox VM to run Docker containers"
 VM_NAME="nifi-playground"
 CPU_COUNT=4
 DISK_SIZE_MB=60000
-MEM_MB=8024
+MEM_MB=12024
 
 DM_CMD="docker-machine create -d virtualbox --virtualbox-disk-size $DISK_SIZE_MB --virtualbox-memory $MEM_MB --virtualbox-cpu-count $CPU_COUNT --engine-insecure-registry docker.jeremydyer.me $VM_NAME"
 echo "Running $DM_CMD"
